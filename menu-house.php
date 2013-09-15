dbdd8c4b8343912943906bac96cba83f8cebb946
André Meyerjürgens <ameyerjuergens@gmail.com>
2013-09-15 17:42:13 +0200

    Menüpunkte Sehenswertes abgesetzt

--- menu-house.php
+++ menu-house.php
@@ -7,6 +7,8 @@
         <li <?php if ($site == 'food') {echo 'class="active"'; } ?> ><a href="/index.php?s=food&m=house">Edith kocht</a></li>
         <li <?php if ($site == 'prices') {echo 'class="active"'; } ?> ><a href="/index.php?s=prices&m=house">Preise</a></li>
         <li <?php if ($site == 'drive') {echo 'class="active"'; } ?> ><a href="/index.php?s=drive&m=house">Anfahrt und Kontakt</a></li>
-        <li <?php if ($site == 'drive') {echo 'class="active"'; } ?> ><a href="/index.php?s=main&m=sights">Sehenswertes und Ausflüge</a></li>
+        </ul>
+    <ul class="nav nav-tabs nav-stacked">
+        <li><a href="/index.php?s=main&m=sights">Sehenswertes und Ausflüge</a></li>
     </ul>
 </div>
