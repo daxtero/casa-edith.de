393e4b1fed186706e583939e8a09e490968b04d1
André Meyerjürgens <ameyerjuergens@gmail.com>
2013-09-15 17:35:08 +0200

    Haus und Umgebung => Bilder hinzugefügt

--- house-main.php
+++ house-main.php
@@ -1,13 +1,6 @@
 <div class="span10 main">
     <h2>Das Haus und die Umgebung</h2>
-    <div style = "float: left">
-        <a href="images/img-1.jpeg" class="highlightit" rel="lightbox-group1" title="Das Haus">
-            <img style="width:40%;height=40%;" src="images/img-1.jpeg"/>
-        </a>
-        <a href="images/img-2.jpeg" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
-            <img style="width:40%;height=40%;" src="images/img-2.jpeg"/>
-        </a>
-    </div>
+
     <p>
         Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes
         demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus,
@@ -15,4 +8,46 @@
         parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.
         Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
     </p>
+
+    <div>
+        <a href="images/house/D2DX5926.jpg" class="highlightit" rel="lightbox-group1" title="Das Haus">
+            <img src="images/house/D2DX5926.jpg"/>
+        </a>
+    </div>
+
+    <div class="twocolumns">
+        <a href="images/house/IMG_0111.jpeg" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
+            <img src="images/house/IMG_0111.jpeg"/>
+        </a>
+    </div>
+    <div class="twocolumns">
+        <a href="images/house/IMG_0153-1.JPG" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
+            <img src="images/house/IMG_0153-1.JPG"/>
+        </a>
+    </div>
+    <div style="clear: left;"></div>
+
+    <div class="twocolumns">
+        <a href="images/house/IMG_0154.jpeg" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
+            <img src="images/house/IMG_0154.jpeg"/>
+        </a>
+    </div>
+    <div class="twocolumns">
+        <a href="images/house/DSC_0628.jpg" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
+            <img src="images/house/DSC_0628.jpg"/>
+        </a>
+    </div>
+    <div style="clear: left;"></div>
+    <div class="twocolumns">
+        <a href="images/house/IMG_0401.jpeg" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
+            <img src="images/house/IMG_0401.jpeg"/>
+        </a>
+    </div>
+    <div class="twocolumns">
+        <a href="images/house/IMG_0278.JPG" class="highlightit" rel="lightbox-group1" title="Der Wasserfall hinter dem Haus">
+            <img src="images/house/IMG_0278.JPG"/>
+        </a>
+    </div>
+    <div style="clear: left;"></div>
+
 </div>
